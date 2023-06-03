56481bc7eeaffe60032e8eabda2fb15888936aeb
elaydg <elayd.gg@gmail.com>
2023-06-04 00:09:31 +0300

    deneme22

--- ortak_sayfalar/profil.php
+++ ortak_sayfalar/profil.php
@@ -16,28 +16,37 @@
             <ul class="dropdown-menu">
               <!-- User image -->
               <li class="user-header">
-                <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
 
+            <?php if(isset($_SESSION['login']) == "true") { ?>
+                <img src="images/users/<?php echo $_SESSION['user_pp'];?>" class="img-circle" alt="User Image">
                 <p>
-                  Alexander Pierce - Web Developer
-                  <small>Member since Nov. 2012</small>
+                    <?php echo $_SESSION['user_name'];?>
+                    <small> Logged in </small>
                 </p>
+
+
+            <?php }else { ?>
+                <img src="dist/img/defaultprofile.png" class="img-circle" alt="User Image">
+                <p>
+                  Guest Member
+                  <small> Not Logged In </small>
+                </p>
+            <?php } ?>
+
               </li>
               <!-- Menu Body -->
-              <li class="user-body">
-                <div class="row">
-                  <div class="col-xs-4 text-center">
-                    <a href="#">Followers</a>
-                  </div>
-                  <div class="col-xs-4 text-center">
-                    <a href="#">Sales</a>
-                  </div>
-                  <div class="col-xs-4 text-center">
-                    <a href="#">Friends</a>
-                  </div>
-                </div>
-                <!-- /.row -->
-              </li>
+                <li class="user-body">
+                    <div class="row">
+                        <div class="col-xs-4 text-center">
+                            <a href="#">Followers</a>
+                        </div>
+
+                        <div class="col-xs-4 text-center">
+                            <a href="#">Friends</a>
+                        </div>
+                    </div>
+                    <!-- /.row -->
+                </li>
               <!-- Menu Footer-->
               <li class="user-footer">
                 <div class="pull-left">
